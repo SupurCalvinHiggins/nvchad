11000bb54b0033d64c8696c7d8019ffcaa520e7a
Calvin Higgins <calvin_higgins2@uri.edu>
2024-07-26 13:35:51 -0400

    Added LSP formatting.

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,7 +1,31 @@
 -- Load default LSP configuration.
 local config = require("plugins.configs.lspconfig")
 
-local on_attach = config.on_attach
+-- Add format on write.
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(client, bufnr)
+  -- Call default NVChad on_attach.
+  config.on_attach(client, bufnr)
+
+  -- Fix bug in default NVChad on_attach.
+  client.server_capabilities.documentFormattingProvider = true
+  client.server_capabilities.documentRangeFormattingProvider = true
+
+  -- Add format on write.
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({
+      group = augroup,
+      buffer = bufnr,
+    })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
+  end
+end
 local capabilities = config.capabilities
 
 -- Configure LSP servers.
@@ -9,12 +33,11 @@ local lspconfig = require("lspconfig")
 
 -- Configure Python LSP.
 lspconfig.pyright.setup({
-  on_attach=on_attach,
-  capabilities=capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 })
 
--- Configure C-like LSP.
 lspconfig.clangd.setup({
-  on_attach=on_attach,
-  capabilities=capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 })
